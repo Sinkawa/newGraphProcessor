d2c5ec16e1fff562079e8a0554944e7a53ff7ccd
Sinkawa <suvoroff.ar@gmail.com>
2020-11-27 11:08:38 +0300

    Updated File Organization

---
+++ cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/MarkN/CLionProjects/GraphProcessor/gp/Graph.cpp" "C:/Users/MarkN/CLionProjects/GraphProcessor/cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/gp/Graph.cpp.obj"
+  "C:/Users/MarkN/CLionProjects/GraphProcessor/gp/GraphHandler.cpp" "C:/Users/MarkN/CLionProjects/GraphProcessor/cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/gp/GraphHandler.cpp.obj"
+  "C:/Users/MarkN/CLionProjects/GraphProcessor/gp/GraphNode.cpp" "C:/Users/MarkN/CLionProjects/GraphProcessor/cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/gp/GraphNode.cpp.obj"
+  "C:/Users/MarkN/CLionProjects/GraphProcessor/source.cpp" "C:/Users/MarkN/CLionProjects/GraphProcessor/cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/source.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-visual-studio/CMakeFiles/graphProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphProcessor.dir/gp/Graph.cpp.obj"
+  "CMakeFiles/graphProcessor.dir/gp/GraphHandler.cpp.obj"
+  "CMakeFiles/graphProcessor.dir/gp/GraphNode.cpp.obj"
+  "CMakeFiles/graphProcessor.dir/source.cpp.obj"
+  "graphProcessor.exe"
+  "graphProcessor.exe.manifest"
+  "graphProcessor.lib"
+  "graphProcessor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graphProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
